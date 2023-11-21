ad7002ebcaf72e893b27c8406a1beea88f257cee
ggalloro <galloro.giovanni@gmail.com>
2023-11-21 09:23:52 +0000

    Generalizing git diff

--- codey/git-analysis.py
+++ codey/git-analysis.py
@@ -2,5 +2,5 @@ import os
 
 os.chdir('./clone/')
 os.system('ls -lrt')
-os.system('git diff main...fix  --output ../diff.txt')
+os.system('git diff main...HEAD  --output ../diff.txt')
 os.system('cat /workspace/diff.txt')
